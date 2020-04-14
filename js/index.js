93e038f52a2a48813866cff58a07a2364033d5ad
bouali zakariae <bouali_zakariae@hotmail.fr>
2020-04-14 22:36:06 +0200

    create Testimonials custom slider using jQuery.

--- js/index.js
+++ js/index.js
@@ -28,3 +28,28 @@ $('.links a').on('click', function () {
     }, 1000);
 });
 
+
+//testimonial custom slider
+(function autoSlider() {
+
+    $('.custom-slider .active').each(function () {
+        if (!$(this).is(':last-child')){
+            $(this).delay(3000).fadeOut(1500, function () {
+                $(this).removeClass('active');
+                let nextDiv = $(this).next();
+                nextDiv.addClass('active');
+                nextDiv.fadeIn();
+                autoSlider();
+            });
+        }else{
+            $(this).delay(3000).fadeOut(1500, function () {
+                $(this).removeClass('active');
+                let firstChild = $('.custom-slider div').eq(0);
+                firstChild.addClass('active');
+                firstChild.fadeIn();
+                autoSlider();
+            });
+        }
+
+    });
+}());
