a3c72b4575551ec2736a15c300bfe05d89179d31
bouali zakariae <bouali_zakariae@hotmail.fr>
2020-04-23 22:13:39 +0200

    Template 3: creation of the features section.

--- js/index3.js
+++ js/index3.js
@@ -1,2 +1,11 @@
 // height of the header
 $('.header').height($(window).height());
+
+
+
+$('.arrow').on('click', function () {
+    console.log('clicked');
+    $('html').animate({
+        scrollTop: $('.feature').offset().top
+    }, 1000);
+});
