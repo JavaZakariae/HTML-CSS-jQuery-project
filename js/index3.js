5ea6ad40ebb7448724a69acecbf33d3a9b4d32cc
bouali zakariae <bouali_zakariae@hotmail.fr>
2020-04-25 17:38:25 +0200

    Template 3: creation of the our-work section.

--- js/index3.js
+++ js/index3.js
@@ -1,11 +1,12 @@
 // height of the header
 $('.header').height($(window).height());
 
-
-
 $('.arrow').on('click', function () {
-    console.log('clicked');
     $('html').animate({
         scrollTop: $('.feature').offset().top
     }, 1000);
+});
+
+$('.show-more').on('click', function () {
+    $('.hiden').fadeIn(2000);
 });
