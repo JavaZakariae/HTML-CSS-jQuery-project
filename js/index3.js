7b3d3df64f0306c67bdd38d058f27bd9af9a620b
bouali zakariae <bouali_zakariae@hotmail.fr>
2020-04-26 01:27:11 +0200

    Template 3: creation of the testimonials section.

--- js/index3.js
+++ js/index3.js
@@ -8,5 +8,51 @@ $('.arrow').on('click', function () {
 });
 
 $('.show-more').on('click', function () {
-    $('.hiden').fadeIn(2000);
+    $('.items-box .hidden').fadeIn(2000);
 });
+
+//activate/deactivate  arrow
+function checkClients() {
+    console.log('hello');
+    let leftArrow = $('.client .left');
+    let rightArrow = $('.client .right');
+    if($('.client:first').hasClass('active'))
+    {
+        leftArrow.fadeOut();
+    }
+    else{
+        leftArrow.fadeIn();
+    }
+    if($('.client:last').hasClass('active'))
+    {
+        rightArrow.fadeOut();
+        leftArrow.fadeIn();
+    }
+    else{
+        rightArrow.fadeIn();
+    }
+};
+checkClients();
+
+
+$('.testimonial i').on('click', function () {
+    if($(this).hasClass('right')){
+        $('.testimonial .active').fadeOut(1000, function () {
+            $(this).removeClass('active');
+            $(this).next().addClass('active');
+            $(this).next().fadeIn();
+            checkClients();
+        })
+    }
+    else
+    {/*left arrow*/
+        $('.testimonial .active').fadeOut(1000, function () {
+            $(this).removeClass('active');
+            $(this).prev().addClass('active');
+            $(this).prev().fadeIn();
+            checkClients();
+        })
+    }
+});
+
+//
